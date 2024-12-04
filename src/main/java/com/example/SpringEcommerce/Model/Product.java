49ccd5ca7836a7e3d7da12ecd3815ab73e6cb383
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-12-04 08:37:27 +0530

    add product with various parameters

---
+++ src/main/java/com/example/SpringEcommerce/Model/Product.java
@@ -0,0 +1,32 @@
+package com.example.SpringEcommerce.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String brand;
+    private String descreption;
+    private float price;
+    private String category;
+    private int quantity;
+    private Date date;
+
+
+
+}
