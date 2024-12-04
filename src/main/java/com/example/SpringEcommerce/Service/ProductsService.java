5af2e44ea8cfd71f720343294ba52bef6e7afa7f
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-12-04 22:20:48 +0530

    add service for add products

--- src/main/java/com/example/SpringEcommerce/Service/ProductsService.java
+++ src/main/java/com/example/SpringEcommerce/Service/ProductsService.java
@@ -5,12 +5,13 @@ import com.example.SpringEcommerce.Repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProductsService {
     @Autowired
     private ProductRepo productRepo;
-    public void addProduct(Product product) {
-        productRepo.save(product);
-
+    public List<Product> getAllProducts() {
+        return productRepo.findAll();
     }
 }
