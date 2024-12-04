76ed333215123ac9ee87d6533217169b4133f6b4
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-12-04 09:36:41 +0530

    add ProductRepo interface for database operations on Product entity

---
+++ src/main/java/com/example/SpringEcommerce/Repository/ProductRepo.java
@@ -0,0 +1,9 @@
+package com.example.SpringEcommerce.Repository;
+
+import com.example.SpringEcommerce.Model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product,Integer> {
+}
