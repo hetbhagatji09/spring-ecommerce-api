729732cf80108cfc6a45ea462e70bdcc7ee8b66c
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-12-04 22:21:58 +0530

    made api for the get all product as list

--- src/main/java/com/example/SpringEcommerce/Controller/ProductsController.java
+++ src/main/java/com/example/SpringEcommerce/Controller/ProductsController.java
@@ -6,15 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+
 @RestController
 @RequestMapping("/api")
 public class ProductsController {
     @Autowired
     private ProductsService productsService;
-
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
     @GetMapping("/products")
-    public String getAllproducts(){
-        return "all products";
+    public List<Product> getProducts(){
+        return productsService.getAllProducts();
     }
 
 }
