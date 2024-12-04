be112a6cef2120007b8034f8e5b2275d8836ebc1
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-12-04 22:17:25 +0530

    remove the image attributes

--- src/main/java/com/example/SpringEcommerce/Model/Product.java
+++ src/main/java/com/example/SpringEcommerce/Model/Product.java
@@ -4,35 +4,103 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 @Data
-@Entity
+@Entity(name="final_product")
 @NoArgsConstructor
-@AllArgsConstructor
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
+    private String description;
     private String brand;
-    private String descreption;
-    private float price;
+    private BigDecimal price;
     private String category;
-    private int quantity;
-    private Date date;
-    //Stores the MIME type of the image (e.g., "image/png" or "image/jpeg").
-    private String imageType;
-    //Stores the name of the uploaded image file (e.g., "product1.png").
-    private String imageName;
-    @Lob
-    private byte[] imageData;
-    public Product(int id){
-        this.id=id;
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQuantity;
+
+    public int getId() {
+        return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public boolean isProductAvailable() {
+        return productAvailable;
+    }
+
+    public void setProductAvailable(boolean productAvailable) {
+        this.productAvailable = productAvailable;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+    //Stores the MIME type of the image (e.g., "image/png" or "image/jpeg").
+//    private String imageType;
+    //Stores the name of the uploaded image file (e.g., "product1.png").
+//    private String imageName;
+//    @Lob
+//    private byte[] imageData;
 
 
 }
