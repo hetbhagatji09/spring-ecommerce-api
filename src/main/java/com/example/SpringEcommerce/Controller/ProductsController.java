78af73b2c53715cf29c2d84dc9c660a58f4998dd
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-12-05 08:37:01 +0530

    Add Api for adding product and implement service method

--- src/main/java/com/example/SpringEcommerce/Controller/ProductsController.java
+++ src/main/java/com/example/SpringEcommerce/Controller/ProductsController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.swing.text.html.parser.Entity;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -34,8 +36,21 @@ public class ProductsController {
             return new ResponseEntity<>(product,HttpStatus.OK);
 
         }
-        else  return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        else{
+            return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
 
+
+    }
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile imageFile) {
+        Product savedProduct = null;
+        try {
+            savedProduct = productsService.addProduct(product, imageFile);
+            return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+        } catch (IOException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
     }
 
--- src/main/java/com/example/SpringEcommerce/Service/ProductsService.java
+++ src/main/java/com/example/SpringEcommerce/Service/ProductsService.java
@@ -5,7 +5,9 @@ import com.example.SpringEcommerce.Repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,5 +22,13 @@ public class ProductsService {
 
     public Product getProduct(int id) {
         return productRepo.findById(id).orElse(new Product(-1));
+    }
+
+    public Product addProduct(Product product, MultipartFile image) throws IOException {
+        product.setImageName(image.getOriginalFilename());
+        product.setImageType(image.getContentType());
+        product.setImageData(image.getBytes());
+
+        return productRepo.save(product);
     }
 }
